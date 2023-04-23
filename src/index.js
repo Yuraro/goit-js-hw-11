876598dce74ad3b73c2bf24e55cf361c860495fb
Yuraro <romanykyriy@gmail.com>
2023-04-23 11:59:33 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,12 +10,39 @@ refs.form.addEventListener("submit", onSubmit);
 
 function onSubmit(e) {
     e.preventDefault();
-    const inputValue = e.currentTarget.elements.searchQuery.value;
+    const form = e.currentTarget;
+    const inputValue = form.elements.searchQuery.value;
     getPhoto(inputValue).then(({ hits }) => {
         console.log(hits);
 
-        if(hits.length === 0) throw new Error("No data!")
-    }).catch(onError);
+        if (hits.length === 0) throw new Error("No data!")
+        
+        hits.reduce((markup, hits) => createMarkup(hits), "");
+    })
+    .finally(() => form.reset())
+    .catch(onError);
+}
+
+function createMarkup({webformatURL,largeImageURL,tags,likes,views,comments,downloads}) {
+    return `
+    <div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+        <p class="info-item">
+            <b>Likes${likes}</b>
+        </p>
+        <p class="info-item">
+            <b>Views${views}</b>
+        </p>
+        <p class="info-item">
+            <b>Comments${comments}</b>
+        </p>
+        <p class="info-item">
+            <b>Downloads${downloads}</b>
+        </p>
+    </div>
+    </div>
+    `;
 }
 
 function onError(err) {
